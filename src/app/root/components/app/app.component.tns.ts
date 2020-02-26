aa95a8d80ccec86025886bc4ef1cd87022ac6f0a
beerna <beerna@L-7470-2C5GTC2>
2020-02-26 11:28:07 +0100

    Add dark mode support on both android/ios.

--- src/app/root/components/app/app.component.tns.ts
+++ src/app/root/components/app/app.component.tns.ts
@@ -1,7 +1,9 @@
 import {Component} from '@angular/core';
 import {HttpService} from '@arhs/core';
-import {EventData, Page, isAndroid} from '@nativescript/core';
+import {isAndroid, Page} from '@nativescript/core';
 import * as statusBar from 'nativescript-status-bar';
+import {ApplicationEventData, on} from 'tns-core-modules/application';
+import {RouterExtensions} from '@nativescript/angular';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +14,15 @@ import * as statusBar from 'nativescript-status-bar';
 })
 export class AppComponent {
 
-  constructor(private httpService: HttpService) {
+  constructor(private httpService: HttpService, private routerExtensions: RouterExtensions) {
     httpService.rootUrl = 'http://10.0.2.2:8080/';
     AppComponent.hideAndroidStatusBar();
+
+    on('resume', (args: ApplicationEventData) => {
+      if (args.android) {
+        AppComponent.hideAndroidStatusBar();
+      }
+    }, this);
   }
   title: String = 'Sample Tns-Ang App';
 
@@ -22,10 +30,5 @@ export class AppComponent {
     if (isAndroid) {
       statusBar.hide();
     }
-  }
-
-  public onPageLoaded(args: EventData): void {
-    const page = <Page>args.object;
-    page.actionBarHidden = true;
   }
 }
--- src/main.tns.ts
+++ src/main.tns.ts
@@ -1,8 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { platformNativeScriptDynamic } from 'nativescript-angular/platform';
+import {platformNativeScriptDynamic} from 'nativescript-angular/platform';
 import {AppModule} from '@src/app/root/app.module';
-import { on, ApplicationEventData } from 'tns-core-modules/application/application';
-import * as statusBar from 'nativescript-status-bar';
 
 // A traditional NativeScript application starts by initializing global objects,
 // setting up global CSS rules, creating, and navigating to the main page.
@@ -10,9 +8,4 @@ import * as statusBar from 'nativescript-status-bar';
 // A NativeScript Angular app needs to make both paradigms work together,
 // so we provide a wrapper platform object, platformNativeScriptDynamic,
 // that sets up a NativeScript application and can bootstrap the Angular framework.
-on('resume', (args: ApplicationEventData) => {
-    if (args.android) {
-        statusBar.hide();
-    }
-});
 platformNativeScriptDynamic().bootstrapModule(AppModule);
